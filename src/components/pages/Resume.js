5d82060d62127ea08d09bbfe6e1589f08941d57a
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-04 23:51:48 -0800

    complete Resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,36 @@
 import React from "react";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <div className="text-left">
+      <h4>Resume</h4>
+      <p>
+        Download my{" "}
+        <a href="/Resume.pdf" dowmload="resume">
+          resume
+        </a>
+        <h5>Front-end proficiencies</h5>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Responsive Design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h5>Back-end proficiencies</h5>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDb, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
+      </p>
+    </div>
+  );
 }
 export default Resume;
