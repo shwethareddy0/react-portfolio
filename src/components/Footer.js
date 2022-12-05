7ec015c464d3b23981d4ec8468b0ae30da6bfa58
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-04 23:00:31 -0800

    update Footer component with Icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,29 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <p>GitHub</p>
-      <p>LinkedIn</p>
-      <p>Twitter/Medium</p>
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a
+            className="nav-link active"
+            href="https://github.com/shwethareddy0"
+          >
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="nav-link"
+            href="https://www.linkedin.com/in/swetha-pothuganti-584252202/"
+          >
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="https://twitter.com/shwetha88018726">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 function AboutMe() {
-  return <div>About me</div>;
+  return (
+    <div>
+      <h4>About me</h4>
+      <p>
+        Student at UC Berkeley Boot Camps. Human Resource professional with 5
+        years of experience in all the core Human Resource operations that
+        includes Recruitment & Selection, Employee Relations, Payrolls and
+        Performance Management.
+      </p>
+    </div>
+  );
 }
 export default AboutMe;
